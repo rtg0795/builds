45797585bfee967c6169a698081900aae47074c0
Raviteja Gorijala <36429068+rtg0795@users.noreply.github.com>
2025-02-22 06:55:34 +0000

    add script.sh

---
+++ script.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e -x
+
+arch=$(uname | tr '[:upper:]' '[:lower:]')
+arch_set="arm64"
+
+ver=6.5.0
+quiet="yes"
+
+if [[ "$quiet" == "yes"]]; then
+    CURL_QUIET="-q"
+    lOG_TO_DEV_NULL="> /dev/null 2>&1"
+fi
+
+retry_curl="--retry 4 --retry-max-out-time 10 --retry-connrefused"
+bazelroot="/usr/local/lib/bazel-${ver}"
+
+set_bazel() {
+    sudo ln -sf "${bazelroot}/bin/bazel" "/usr/local/bin/bazel"
+    exit 0
+}
+
+if [[ -e "${bazelroot}/bin/bazel" ]]; then
+    set_bazel
+fi
+
+filename="bazel-${ver}-installer-${arch}-${arch_set}.sh"
+cmd="curl $CURL_QUIET -sLO $retry_curl \
+    https://storage.googleapis.com/bazel/${ver}/release/${filename}"
+
+eval "$cmd
+sudo mkdir -p "${bazelroot}"
+chmod 755 "${filename}"
+command="sudo \"./${filename}\" \"--prefix=${bazelroot}\" $LOG_TO_DEV_NULL"
+eval "$command"
+rm -rf "${filename}"
+set_bazel
