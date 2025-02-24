78ba1d75f226ed02ae986beaaa14e736c8ef6525
Raviteja Gorijala <36429068+rtg0795@users.noreply.github.com>
2025-02-24 20:09:03 +0000

    change

--- script.sh
+++ script.sh
@@ -5,12 +5,6 @@ arch=$(uname | tr '[:upper:]' '[:lower:]')
 arch_set="arm64"
 
 ver="6.5.0"
-quiet="yes"
-
-if [[ "${quiet}" == "yes" ]]; then
-    CURL_QUIET="-q"
-    lOG_TO_DEV_NULL="> /dev/null 2>&1"
-fi
 
 retry_curl="--retry 4 --retry-max-time 10 --retry-connrefused"
 bazelroot="/usr/local/lib/bazel-${ver}"
@@ -25,7 +19,7 @@ if [[ -e "${bazelroot}/bin/bazel" ]]; then
 fi
 
 filename="bazel-${ver}-installer-${arch}-${arch_set}.sh"
-cmd="curl $CURL_QUIET -sLO $retry_curl https://storage.googleapis.com/bazel/${ver}/release/${filename}"
+cmd="curl -q -sLO $retry_curl https://storage.googleapis.com/bazel/${ver}/release/${filename}"
 
 eval "$cmd"
 sudo mkdir -p "${bazelroot}"
@@ -34,3 +28,37 @@ command="sudo \"./${filename}\" \"--prefix=${bazelroot}\" $LOG_TO_DEV_NULL"
 eval "$command"
 rm -rf "${filename}"
 set_bazel
+
+
+export BUILD_ENV=/tmpfs/BUILD_ENV
+mkdir -p ${BUILD_ENV}
+
+JAVA_HOME=$(/usr/libexec/java_home -v21)
+export JAVA_HOME
+java -version
+
+# pyenv
+PYENV_ROOT="/Users/kbuilder/.pyenv"
+cd "${PYENV_ROOT}" && git pull && cd -
+eval "${pyenv init -}"
+
+pyenv install -s 3.11.1
+pyenv shell 3.11.1
+PYTHON_BIN_PATH="/Users/kbuilder/.pyenv/versions/3.11.1/bin/python"
+PIP_COMMAND="$(pyenv which pip)"
+"${PYTHON_BIN_PATH} --version"
+"${PIP_COMMAND} --version"
+
+"${PYTHON_BIN_PATH}" -m venv "${BUILD_ENV}"
+source "${BUILD_ENV}/bin/activate"
+
+PYTHON_BIN_PATH=$(which python)
+PIP_COMMAND=$(which pip)
+
+"${PIP_COMMAND} install pip --upgrade --quiet"
+export SETUPTOOLS_USE_DISUTILS=stdlib
+
+install_targets+=("wheel==0.42.0")
+install_targets+=("setuptools" "pipdeptree")
+"${PIP_COMMAND}" install "${install_targets[@]}" --upgrade --quiet
+"${PIP_COMMAND}" list
